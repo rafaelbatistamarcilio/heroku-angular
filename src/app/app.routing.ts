15f5a4ad1057a8db933fed45ebdace16de78f0bb
Rafael Marcilio <rafaelbatistamarcilio@gmail.com>
2018-09-02 00:05:46 -0300

    routes added

---
+++ src/app/app.routing.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '404', component: NotFoundComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+    { path: '**', redirectTo: '404' }
+
+];
+
+
+@NgModule({
+    exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+})
+export class AppRoutingModule { }
