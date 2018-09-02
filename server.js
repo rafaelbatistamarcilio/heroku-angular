92efb2c7ca05fb2458d0fa6a2afefa4b2496a579
Rafael Marcilio <rafaelbatistamarcilio@gmail.com>
2018-09-02 10:57:56 -0300

    compression config

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
-// const compression = require('compression');
+const compression = require('compression');
 
 const app = express();
 
-// app.use(compression());
+app.use(compression());
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist'));
+
+//body parsing middleware (for json, urlencoded, and multipart responses)
+app.use(express.bodyParser());
+
+//handle any errors
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, err);
+});
+
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join('./dist/index.html'));
