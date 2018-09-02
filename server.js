732cad4fec1ff51c71e1a8de1da98654f36b4590
Rafael Marcilio <rafaelbatistamarcilio@gmail.com>
2018-09-02 10:34:05 -0300

    compression config

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ const compression = require('compression');
 
 const app = express();
 
-app.use(compression());
+app.use(compression({
+  filter: (req, res) => {
+    if (req.headers['x-no-compression']) {
+      // don't compress responses with this request header
+      return false
+    }
+
+    // fallback to standard filter function
+    return compression.filter(req, res)
+  }
+}));
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist'));
 
-app.get('/*', function(req,res) {
-    res.sendFile(path.join('./dist/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join('./dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
