5928237b815ab6eb8881ac94d7c9dab81645b3fe
Rafael Marcilio <rafaelbatistamarcilio@gmail.com>
2018-09-02 17:19:36 -0300

    app color edit

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use(forceSSL());
 
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist'));
+app.use(express.static('dist') );
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join('./dist/index.html'));
+app.get('*', function (req, res, next) {
+  res.sendFile(path.resolve('dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,8 +5,10 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
     { path: '404', component: NotFoundComponent },
+
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
 
     { path: '**', redirectTo: '404' }
 
